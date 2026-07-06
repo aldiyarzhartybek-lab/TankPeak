3de2e258a74611ddff084a8da21f59f92cbe8d7e
aldiyarzhartybek-lab <aldiyar.zhartybek@gmail.com>
2026-07-06 21:20:45 +0500

    MiniRefact if

--- MTstat/Services/PlayerAnalyzer.cs
+++ MTstat/Services/PlayerAnalyzer.cs
@@ -2,24 +2,27 @@
 namespace MTstat.Services;
 
 public class PlayerAnalyzer : IPlayerAnalyzer
-{    
+{
+    private const double MinWinRate = 49;
+    private const double MinSurvived = 40;
+    private const int MinAvgDamage  = 1800;
     public AnalysisResult Analyze(PlayerStats playerStats)
     {
         var result = new AnalysisResult();
         result.WinRate = Math.Round((double) playerStats.Wins / playerStats.Battles * 100 ,2) ;
         result.Survived = Math.Round((double) playerStats.SurvivedBattles / playerStats.Battles * 100 ,2) ;
         result.AvgDamage = playerStats.DamageDealt / playerStats.Battles;
-        if (result.WinRate < 49)
+        if (result.WinRate < MinWinRate)
         {
             result.Weaknesses.Add("Низкий винрейт");
         }
 
-        if (result.Survived < 30)
+        if (result.Survived < MinSurvived)
         {
             result.Weaknesses.Add("Низкая выживаемость");
         }
 
-        if (result.AvgDamage < 2000)
+        if (result.AvgDamage < MinAvgDamage)
         {
             result.Weaknesses.Add("Малый средний урон");
         }
